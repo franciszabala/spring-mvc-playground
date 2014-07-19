e03e15703c74e6762f84d5d4db04610ae759b014
franciszabala <francis.zabala@yahoo.com>
2014-07-19 16:03:29 +0800

    More changes

--- src/com/example/controller/HelloWorldController.java
+++ src/com/example/controller/HelloWorldController.java
@@ -2,6 +2,8 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,5 +13,27 @@ public class HelloWorldController {
 	public String sampleMethod1() {
 		return "destination";
 	}
+	
+//	@RequestMapping (value="/login", method=RequestMethod.GET)
+//	public String setupLogin(Model model) {
+//		LoginFromBean formBean = new LoginFromBean();
+//		
+//	}
+	
+//	@RequestMapping (value="/sampleParameterOnUrl/{samparam}")
+//	public String sampleParameterOnUrl {
+//		@ModelAttribute(value="samparam") String param, Model model) {
+//			
+//		}
+//		
+//	}
+	//NOT AVAILABLE IN SPRING 3
+	@RequestMapping(value="/sampleParameterOnList/{samparam}")
+	public String sampleParameterOnList (
+			@PathVariable String samparam, Model model) {
+		model.addAttribute("message", samparam);
+		return "destination";
+	}
+	
 
 }
