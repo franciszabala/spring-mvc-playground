597d2dc6f6cc6f45a0c00c5a841abb191bd4fa39
franciszabala <francis.zabala@yahoo.com>
2014-07-19 18:06:32 +0800

    messages bundle

--- src/com/example/controller/LoginFormBean.java
+++ src/com/example/controller/LoginFormBean.java
@@ -4,11 +4,16 @@ public class LoginFormBean {
 	
 	private LoginCredentials userLogin = new LoginCredentials();
 	
+	//if null, jsp can't find it
+	//it has to exist even if it's empty
+	//results in a null pointer exception in jsp
+	//private LoginCredentials userLogin;
+	
 	//get/set userLogin is tied to the code in jsp
 	public LoginCredentials getUserLogin() {
 		return userLogin;
 	}
-	
+	//get and setter used by jsp, and it looks like it's accessing a public field
 	public void setuserLogin(LoginCredentials userLogin){
 		this.userLogin = userLogin;
 		
